f62d0f7bca5c28881cdbac71a7301c831ff7a13c
Daniel Collin <daniel@collin.com>
2025-01-14 11:01:21 +0100

    Added docs to Padding

--- src/layout/padding.rs
+++ src/layout/padding.rs
@@ -1,14 +1,20 @@
 use crate::bindings::*;
 
+/// Represents padding with values for left, right, top, and bottom.
 #[derive(Debug, Copy, Clone, Default)]
 pub struct Padding {
+    /// Padding value for the left side.
     pub left: u16,
+    /// Padding value for the right side.
     pub right: u16,
+    /// Padding value for the top side.
     pub top: u16,
+    /// Padding value for the bottom side.
     pub bottom: u16,
 }
 
 impl Padding {
+    /// Creates a new `Padding` instance with specific values for all sides.
     pub fn new_rect(left: u16, right: u16, top: u16, bottom: u16) -> Self {
         Self {
             left,
@@ -18,42 +24,51 @@ impl Padding {
         }
     }
 
+    /// Creates a new `Padding` instance with the same value for all sides.
     pub fn all(value: u16) -> Self {
         Self::new_rect(value, value, value, value)
     }
 
+    /// Creates a new `Padding` instance with the same value for left and right sides.
     pub fn horizontal(value: u16) -> Self {
         Self::new_rect(value, value, 0, 0)
     }
 
+    /// Creates a new `Padding` instance with the same value for top and bottom sides.
     pub fn vertical(value: u16) -> Self {
         Self::new_rect(0, 0, value, value)
     }
 
+    /// Creates a new `Padding` instance with default values (all zero).
     pub fn new() -> Self {
         Self::default()
     }
 
+    /// Sets the padding value for the left side.
     pub fn left(&mut self, value: u16) -> &mut Self {
         self.left = value;
         self
     }
 
+    /// Sets the padding value for the right side.
     pub fn right(&mut self, value: u16) -> &mut Self {
         self.right = value;
         self
     }
 
+    /// Sets the padding value for the top side.
     pub fn top(&mut self, value: u16) -> &mut Self {
         self.top = value;
         self
     }
 
+    /// Sets the padding value for the bottom side.
     pub fn bottom(&mut self, value: u16) -> &mut Self {
         self.bottom = value;
         self
     }
 
+    /// Finalizes the modifications and returns the updated `Padding` instance.
     pub fn end(&mut self) -> Self {
         *self
     }
